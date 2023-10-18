725bc270049adbaff15f5b153dfae4cdf9792466
dvitkovic <dvitkovic@pbl.ca>
2023-10-18 21:09:08 +0200

    Initial commit

---
+++ src/main/java/com/java/keycloakextension/KeycloakExtensionApplication.java
@@ -0,0 +1,13 @@
+package com.java.keycloakextension;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeycloakExtensionApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(KeycloakExtensionApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/java/keycloakextension/KeycloakExtensionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.keycloakextension;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeycloakExtensionApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
